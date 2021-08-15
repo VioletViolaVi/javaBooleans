652e730e89322b01e20a8a70013bcfe7b698ef52
VioletViola <vivian.v18723@gmail.com>
2021-08-15 17:52:59 +0100

    Practice using Boolean.parseBoolean().

--- src/Booleans.java
+++ src/Booleans.java
@@ -8,6 +8,17 @@ public class Booleans {
         System.out.println("2 > 7 is " + (2 > 7));
         System.out.println("3 >= 3 is " + (3 >= 3));
         System.out.println("20 <= 40 is " + (20 <= 40));
+        System.out.println("\"89\".length() > 9 is " + ("89".length() > 9));
+        /*  Below Boolean.parseBoolean("TrUE")
+            tries to make sense of 'TrUE' and
+            interprets it as the boolean value
+            of 'true' as long as 'true' can be
+            found. If 'true' cannot be found
+            then 'false' will be returned.
+        */
+        System.out.println("Boolean.parseBoolean(\"TrUE\") is " + Boolean.parseBoolean("TrUE"));
+        System.out.println("Boolean.parseBoolean(\"false\") is " + Boolean.parseBoolean("false"));
+        System.out.println("Boolean.parseBoolean(\"This will show up as false\") is " + Boolean.parseBoolean("This will show up as false"));
 
     }
 }
