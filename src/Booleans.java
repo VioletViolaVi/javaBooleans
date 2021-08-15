5c5a0ba183ff0cc49727f9b8df98ba38c5540caf
VioletViola <vivian.v18723@gmail.com>
2021-08-15 17:25:07 +0100

    Add examples of comparative operators.

--- src/Booleans.java
+++ src/Booleans.java
@@ -1,6 +1,13 @@
 public class Booleans {
     public static void main(String[] args) {
-        System.out.println(true);
-        System.out.println(false);
+        System.out.println("true is " + true);
+        System.out.println("false is " + false);
+        System.out.println("10 == 10 is " + (10 == 10));
+        System.out.println("4 != 4.4 is " + (4 != 4.4));
+        System.out.println("5 < 6 is " + (5 < 6));
+        System.out.println("2 > 7 is " + (2 > 7));
+        System.out.println("3 >= 3 is " + (3 >= 3));
+        System.out.println("20 <= 40 is " + (20 <= 40));
+
     }
 }
